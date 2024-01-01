69d03cf122ef61ac259c4be1a62c6c436fef86c5
MohammedSolyman <MohammedSolyman10@gmail.com>
2024-01-01 14:01:23 +0200

    avoid obsecured bystatus bar, store dimensions localy were performed

--- lib/controllers/dimensions_controller.dart
+++ lib/controllers/dimensions_controller.dart
@@ -1,5 +1,6 @@
 import 'package:funny_tic_tac_toe/models/dimensions_model.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class DimensionsController extends GetxController {
   //this controller is responsible of device dimensions of the whole app.
@@ -9,14 +10,39 @@ class DimensionsController extends GetxController {
       {required double width,
       required double height,
       required double statusBarHeight}) {
-    if (width != 0 && height != 0 && statusBarHeight != 0) {
+    GetStorage box = GetStorage();
+
+    //1. if the dimensions are already stored
+    if (box.read<double>('width') != null &&
+        box.read<double>('height') != null &&
+        box.read<double>('statusBarHeight') != null) {
       model.update((val) {
-        val!.height = height;
-        val.width = width;
-        val.statusBarHeight = statusBarHeight;
+        // 1.1 update the values
+        val!.height = box.read<double>('height')!;
+        val.width = box.read<double>('width')!;
+        val.statusBarHeight = box.read<double>('statusBarHeight')!;
         print(
-            '---from media query  (${val.height} ,${val.width}, ${val.statusBarHeight} )');
+            '---from storage  (${val.height} ,${val.width}, ${val.statusBarHeight} )');
       });
+    }
+
+    //2. if the dimensions are NOT stored (first time)
+    else {
+      if (width != 0 && height != 0 && statusBarHeight != 0) {
+        // 2.1 update the values
+        model.update((val) {
+          val!.height = height;
+          val.width = width;
+          val.statusBarHeight = statusBarHeight;
+          print(
+              '---from media query  (${val.height} ,${val.width}, ${val.statusBarHeight} )');
+        });
+
+        // 2.2 store values locally
+        box.write('width', width);
+        box.write('height', height);
+        box.write('statusBarHeight', statusBarHeight);
+      }
     }
   }
 }
